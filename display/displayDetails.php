586eef2f6d9714efe69e9dd9fc74445ad9caf1d0
EmpressKing143 <reynandalida@gmail.com>
2024-04-22 17:58:34 +0800

    update fritz3

--- display/displayDetails.php
+++ display/displayDetails.php
@@ -3,23 +3,34 @@
     $username = $_SESSION['email'] = $_POST['username'];
     $email = $_SESSION['email'] = $_POST['email'];
     $password = $_SESSION['password'] = $_POST['password'];
+
+    function showDetails() {
+        if (!$username = " "){
+            $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
+            echo $username."<br>";
+        } else {
+            echo " The username is empty". "<br>";
+        }
+        
+        $email = filter_var($_POST["email"], FILTER_SANITIZE_STRING);
+        if ($email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+            echo $email."<br>";
+        } else {
+            echo "The email is invalid"."<br>";
+        }
+        
+        $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
+        if ($password = " "){
+            echo "The password is empty"."<br>";
+        } else {
+            echo $password."<br>";
+        }
+        }
+            
+        
 ?>
 
-<?php 
-if (empty($username)){
-    echo " The username is empty". "<br>";
-} else {
-    $username = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-    echo $username."<br>";
-}
-$email = filter_var($_POST["email"], FILTER_SANITIZE_STRING);
-if ($email = filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
-    echo $email."<br>";
-} else {
-    echo "The email is invalid"."<br>";
-}
-    echo $password."<br>";
-?>
+
 
 
 <a href="../index.php"> Go Back </a>
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
-// session_start(); //hello reynan
+session_start(); //hello reynan
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//     $_SESSION['username'] = $_POST['username'] ?? '';
-//     $_SESSION['email'] = $_POST['email'] ?? '';
-//     $_SESSION['password'] = $_POST['password'] ?? '';
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $_SESSION['username'] = $_POST['username'] ?? '';
+    $_SESSION['email'] = $_POST['email'] ?? '';
+    $_SESSION['password'] = $_POST['password'] ?? '';
 
-//     header("Location: loginmodule/addinfo.php");
-//     exit;
-//}
+    header("Location: loginmodule/addinfo.php");
+    exit;
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <?php require("loginmodule/head.php"); ?>
@@ -18,7 +19,7 @@
     <div class="container">
         <div class="index-form">
             <h2>Sign Up</h2>
-            <form action=" display/displayDetails.php <?php //echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <form action=" <?php //echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                 <div class="form-group">
                     <div class="input-icon">
                         <span class="icon"><i class="fa-solid fa-user" style="color: #485c66;"></i></span>
@@ -37,10 +38,13 @@
                         <input type="password" id="password" name="password" placeholder="Password" required>
                     </div>
                 </div>
-                <button type="submit">Create Account</button>
+                <button type="submit"> Create Account </button>
+               
             </form>
             <p>Already have an account? <a href="loginmodule/login.php">Login!</a></p>
         </div>
     </div>
 </body>
 </html>
+<?php 
+
