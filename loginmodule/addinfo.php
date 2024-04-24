f7abc232e709cab2bfb261a9703a6dccea41e146
EmpressKing143 <reynandalida7@gmail.com>
2024-04-24 17:05:31 +0800

    kk

--- loginmodule/addinfo.php
+++ loginmodule/addinfo.php
@@ -40,6 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 ?>
 <!DOCTYPE html>
 <html lang="en">
+    <head><link rel="icon" type="image" href="../image/icon.png"></head>
 <?php require("head.php"); ?>
 <body>
     <?php require("header.php"); ?>
--- loginmodule/head.php
+++ loginmodule/head.php
@@ -6,5 +6,5 @@
     <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="css/font-awesome-pro-main/fontawesome-pro-6.5.1-web/css/all.min.css">
     <link rel="stylesheet" href="/sugarrush/css/login.css">
-    <link rel="icon" href="../image/icon.png" type="image/x-icon"/>
+    <link rel="icon" type="image" href="image/icon.png">
 </head>
