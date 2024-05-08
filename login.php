81ef8c546b2280f762d8788f2aea982c25e57ecf
EmpressKing143 <reynandalida@gmail.com>
2024-05-09 02:07:14 +0800

    1

---
+++ login.php
@@ -0,0 +1,85 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+if(isset($_POST['submit'])){
+
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_EMAIL); // Changed to FILTER_SANITIZE_EMAIL for email validation
+   $pass = $_POST['pass'];
+
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+   $select_user->execute([$email]);
+   $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+   if($select_user->rowCount() > 0){
+      // Verify the password
+      if($pass == $row['password']){
+         $_SESSION['user_id'] = $row['id'];
+         header('location: home.php');
+      } else {
+         $message[] = 'Incorrect username or password!';
+      }
+   }else{
+      $message[] = 'Incorrect username or password!';
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Login</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'components/user_header.php'; ?>
+<!-- header section ends -->
+
+<section class="form-container">
+
+   <form action="" method="post">
+      <h3>Login Now</h3>
+      <?php
+      if(isset($message)) {
+         foreach($message as $msg) {
+            echo "<p class='error'>$msg</p>";
+         }
+      }
+      ?>
+      <input type="email" name="email" required placeholder="Enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="password" name="pass" required placeholder="Enter your password" class="box" maxlength="50">
+      <input type="submit" value="Login Now" name="submit" class="btn">
+      <p>Don't have an account? <a href="register.php">Register Now</a></p>
+   </form>
+
+</section>
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,95 @@
+<?php
+
+include 'components/connect.php';
+
+session_start();
+
+if(isset($_SESSION['user_id'])){
+   $user_id = $_SESSION['user_id'];
+}else{
+   $user_id = '';
+};
+
+if(isset($_POST['submit'])){
+
+   $name = $_POST['name'];
+   $name = filter_var($name, FILTER_SANITIZE_STRING);
+   $email = $_POST['email'];
+   $email = filter_var($email, FILTER_SANITIZE_STRING);
+   $number = $_POST['number'];
+   $number = filter_var($number, FILTER_SANITIZE_STRING);
+   $pass = $_POST['pass'];
+   $cpass = $_POST['cpass'];
+
+   // Store the password as plain text
+   $plain_password = $pass;
+
+   $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ? OR number = ?");
+   $select_user->execute([$email, $number]);
+   $row = $select_user->fetch(PDO::FETCH_ASSOC);
+
+   if($select_user->rowCount() > 0){
+      $message[] = 'Email or number already exists!';
+   }else{
+      if($pass != $cpass){
+         $message[] = 'Confirm password not matched!';
+      }else{
+         $insert_user = $conn->prepare("INSERT INTO `users`(name, email, number, password) VALUES(?,?,?,?)");
+         $insert_user->execute([$name, $email, $number, $plain_password]); // Store plain password
+         $select_user = $conn->prepare("SELECT * FROM `users` WHERE email = ?");
+         $select_user->execute([$email]);
+         $row = $select_user->fetch(PDO::FETCH_ASSOC);
+         if($select_user->rowCount() > 0){
+            $_SESSION['user_id'] = $row['id'];
+            header('location: home.php');
+         }
+      }
+   }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Register</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'components/user_header.php'; ?>
+<!-- header section ends -->
+
+<section class="form-container">
+
+   <form action="" method="post">
+      <h3>Register Now</h3>
+      <input type="text" name="name" required placeholder="Enter your name" class="box" maxlength="50">
+      <input type="email" name="email" required placeholder="Enter your email" class="box" maxlength="50" oninput="this.value = this.value.replace(/\s/g, '')">
+      <input type="number" name="number" required placeholder="Enter your number" class="box" min="0" max="9999999999" maxlength="10">
+      <input type="password" name="pass" required placeholder="Enter your password" class="box" maxlength="50">
+      <input type="password" name="cpass" required placeholder="Confirm your password" class="box" maxlength="50">
+      <input type="submit" value="Register Now" name="submit" class="btn">
+      <p>Already have an account? <a href="login.php">Login now</a></p>
+   </form>
+
+</section>
+
+<?php include 'components/footer.php'; ?>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
