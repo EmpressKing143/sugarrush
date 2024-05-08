753822f858b7366dda46652b0f505a2acfec06c0
EmpressKing143 <reynandalida@gmail.com>
2024-05-08 15:46:35 +0800

    kkk

---
+++ main/home.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html>
+<head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="initial-scale=1, width=device-width">
+  	
+  	<link rel="stylesheet" href="../css/font-awesome-pro-main/fontawesome-pro-6.5.1-web/css/all.min.css">
+  	<link rel="stylesheet"  href="../css/home.css" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus Jakarta Sans:wght@300;500;600&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair Display:wght@500;600;900&display=swap" />
+</head>
+<body>
+  	
+  	<header> <!-- Background -->
+      <div class="rectangle-div">
+      <div class="home-child1"></div>
+      <div class="rectangle-parent">
+        <div class="group-child"></div>
+        <div class="search">Search</div>
+        <div class="popular">Home</div>
+        <div class="food">Menu</div>
+        <div class="about-us">About Us</div>
+        <i class="fas fa-search tablersearch-icon"></i>
+        <img class="vector-icon" alt="" src="Vector.svg">
+      </div>
+    </header>
+    <div class="home-child"></div>
+  	<div class="home">
+    	<div class="home-child"></div>
+    	<div class="home-item"></div>
+    	<div class="home-inner"></div>
+    	<div class="home-child1"></div>
+    	<img class="icround-facebook-icon" alt="" src="ic:round-facebook.svg">
+    	
+    	<div class="popular1">Popular</div>
+    	<div class="food1">Food</div>
+    	<div class="about-us1">About Us</div>
+    	
+    	
+    	<div class="sugar-rush-sugar-container">
+      		<p class="sugar-rush">SUGAR RUSH</p>
+      		<p class="sugar-rush">&nbsp;</p>
+      		<p class="sugar-rush">SUGAR RUSH</p>
+      		<p class="sugar-rush">&nbsp;</p>
+      		<p class="sugar-rush">SUGAR RUSH</p>
+      		<p class="sugar-rush">&nbsp;</p>
+      		<p class="sugar-rush">SUGAR RUSH</p>
+      		<p class="sugar-rush">&nbsp;</p>
+      		<p class="sugar-rush">SUGAR RUSH</p>
+    	</div>
+    	<img class="cakedrink-1-icon" alt="" src="../assets/cakedrink.png">
+    	
+    	<div class="indulge-the-sweet-container">
+      		<p class="sugar-rush">Indulge the sweet </p>
+      		<p class="sugar-rush">ecstacy.</p>
+    	</div>
+    	<div class="feel-the-taste">Feel the taste of the most popular desserts from anywhere and anytime</div>
+    	<div class="home-child3"></div>
+        <div class="rectangle-order"></div>
+    	<div class="order-now">Order Now</div>
+    	<div class="home-child4"></div>
+    	<div class="how-to-order">How to Order</div>
+    	<div class="k">2.4k+</div>
+    	<div class="happy-customers">
+      		<p class="sugar-rush">Happy </p>
+      		<p class="sugar-rush">Customers</p>
+    	</div>
+    	<div class="love-the-flavors">“Love the flavors and irresistible textures keep me coming back for more go-to spot for satisfying my sweet tooth.”</div>
+    	<img class="logo1-icon" alt="" src="logo1.png">
+    	
+    	<img class="vector-icon2" alt="" src="../assets/Vector.svg">
+    	
+    	<img class="mditwitter-icon" alt="" src="../assets/mdi_twitter.svg">
+  	</div>
+  	
+</body>
+</html>
