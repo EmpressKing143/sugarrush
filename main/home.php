2b842015a44b9f71b68f04cb256119bfb2839275
EmpressKing143 <reynandalida@gmail.com>
2024-05-09 01:53:25 +0800

    kkk

--- main/home.php
+++ main/home.php
@@ -4,74 +4,86 @@
   	<meta charset="utf-8">
   	<meta name="viewport" content="initial-scale=1, width=device-width">
   	
-  	<link rel="stylesheet" href="../css/font-awesome-pro-main/fontawesome-pro-6.5.1-web/css/all.min.css">
   	<link rel="stylesheet"  href="../css/home.css" />
-  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus Jakarta Sans:wght@300;500;600&display=swap" />
   	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Playfair Display:wght@500;600;900&display=swap" />
+  	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Plus Jakarta Sans:wght@300;500;600&display=swap" />
+  	
+  	
+  	
 </head>
 <body>
   	
-  	<header> <!-- Background -->
-      <div class="rectangle-div">
-      <div class="home-child1"></div>
-      <div class="rectangle-parent">
-        <div class="group-child"></div>
-        <div class="search">Search</div>
-        <div class="popular">Home</div>
-        <div class="food">Menu</div>
-        <div class="about-us">About Us</div>
-        <i class="fas fa-search tablersearch-icon"></i>
-        <img class="vector-icon" alt="" src="Vector.svg">
-      </div>
-    </header>
-    <div class="home-child"></div>
   	<div class="home">
-    	<div class="home-child"></div>
-    	<div class="home-item"></div>
-    	<div class="home-inner"></div>
-    	<div class="home-child1"></div>
-    	<img class="icround-facebook-icon" alt="" src="ic:round-facebook.svg">
-    	
-    	<div class="popular1">Popular</div>
-    	<div class="food1">Food</div>
-    	<div class="about-us1">About Us</div>
-    	
-    	
-    	<div class="sugar-rush-sugar-container">
-      		<p class="sugar-rush">SUGAR RUSH</p>
-      		<p class="sugar-rush">&nbsp;</p>
-      		<p class="sugar-rush">SUGAR RUSH</p>
-      		<p class="sugar-rush">&nbsp;</p>
-      		<p class="sugar-rush">SUGAR RUSH</p>
-      		<p class="sugar-rush">&nbsp;</p>
-      		<p class="sugar-rush">SUGAR RUSH</p>
-      		<p class="sugar-rush">&nbsp;</p>
-      		<p class="sugar-rush">SUGAR RUSH</p>
-    	</div>
-    	<img class="cakedrink-1-icon" alt="" src="../assets/cakedrink.png">
-    	
-    	<div class="indulge-the-sweet-container">
-      		<p class="sugar-rush">Indulge the sweet </p>
-      		<p class="sugar-rush">ecstacy.</p>
-    	</div>
-    	<div class="feel-the-taste">Feel the taste of the most popular desserts from anywhere and anytime</div>
-    	<div class="home-child3"></div>
-        <div class="rectangle-order"></div>
-    	<div class="order-now">Order Now</div>
-    	<div class="home-child4"></div>
-    	<div class="how-to-order">How to Order</div>
-    	<div class="k">2.4k+</div>
-    	<div class="happy-customers">
-      		<p class="sugar-rush">Happy </p>
-      		<p class="sugar-rush">Customers</p>
-    	</div>
-    	<div class="love-the-flavors">“Love the flavors and irresistible textures keep me coming back for more go-to spot for satisfying my sweet tooth.”</div>
-    	<img class="logo1-icon" alt="" src="logo1.png">
-    	
-    	<img class="vector-icon2" alt="" src="../assets/Vector.svg">
-    	
-    	<img class="mditwitter-icon" alt="" src="../assets/mdi_twitter.svg">
+    		<div class="home-child">
+    		</div>
+    		<div class="rectangle-parent">
+      			<div class="group-child">
+      			</div>
+      			<div class="sugar-rush-sugar-container">
+        				<p class="sugar-rush">SUGAR RUSH</p>
+        				<p class="sugar-rush">&nbsp;</p>
+        				<p class="sugar-rush">SUGAR RUSH</p>
+        				<p class="sugar-rush">&nbsp;</p>
+        				<p class="sugar-rush">SUGAR RUSH</p>
+        				<p class="sugar-rush">&nbsp;</p>
+        				<p class="sugar-rush">SUGAR RUSH</p>
+        				<p class="sugar-rush">&nbsp;</p>
+        				<p class="sugar-rush">SUGAR RUSH</p>
+      			</div>
+    		</div>
+    		<div class="rectangle-group">
+      			<div class="group-item">
+      			</div>
+      			<div class="rectangle-container">
+        				<div class="group-inner">
+        				</div>
+        				<div class="search">Search</div>
+        				<div class="popular">Popular</div>
+        				<div class="menu">Menu</div>
+        				<div class="about-us">About Us</div>
+      			</div>
+      			<img class="vector-icon" alt="" src="../assets/Vector.svg">
+      			
+      			<img class="tablersearch-icon" alt="" src="../assets/tabler:search.svg">
+      			
+    		</div>
+    		<img class="cakedrink-1-icon" alt="" src="../assets/cakedrink 1.png">
+    		
+    		<div class="indulge-the-sweet-container">
+      			<p class="sugar-rush">Indulge the sweet </p>
+      			<p class="sugar-rush">ecstacy.</p>
+    		</div>
+    		<div class="feel-the-taste">Feel the taste of the most popular desserts from anywhere and anytime</div>
+    		<div class="home-item">
+    		</div>
+    		<div class="order-now">Order Now</div>
+    		<div class="home-inner">
+    		</div>
+    		<div class="how-to-order">How to Order</div>
+    		<div class="group-div">
+      			<div class="rectangle-div">
+      			</div>
+      			<div class="k">2.4k+</div>
+      			<div class="happy-customers">
+        				<p class="sugar-rush">Happy </p>
+        				<p class="sugar-rush">Customers</p>
+      			</div>
+      			<div class="love-the-flavors">“Love the flavors and irresistible textures keep me coming back for more go-to spot for satisfying my sweet tooth.”</div>
+    		</div>
+    		<div class="rectangle-parent1">
+      			<div class="group-child1">
+      			</div>
+      			<div class="copyright2024-designed-by-container">
+        				<span class="copyright2024-designed-by-container1">
+          					<p class="sugar-rush">Copyright©2024; Designed by Team 43°</p>
+          					<p class="sugar-rush">For Education Purposes Only</p>
+        				</span>
+      			</div>
+      			<img class="logo1-icon" alt="" src="../assets/logo1.png">
+      			
+      			<img class="group-icon" alt="" src="../assets/Group 24.svg">
+      			
+    		</div>
   	</div>
-  	
 </body>
 </html>
